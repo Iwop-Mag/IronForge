3aa0b6c27b3037c50c314ffeebfc062e55ba0eed
Paul <paul.magalong0@gmail.com>
2026-06-07 00:51:46 +0800

    Added modals, not yet polished.

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
+// Get the modal
+var modal = document.getElementById("Modal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("Btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
 (function () {
   'use strict';
 
@@ -56,5 +82,19 @@
       normalizeScroll: true,
       ignoreMobileResize: true
     });
+  }
+
+  if (typeof gsap !== 'undefined') {
+    gsap.fromTo('.nav-links li',
+      { y: -20, opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        duration: 0.5,
+        stagger: 0.1,
+        ease: 'power2.out',
+        delay: 0.3
+      }
+    );
   }
 })();
