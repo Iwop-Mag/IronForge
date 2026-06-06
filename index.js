8f6839185d41383d3e8fec9b812560ddc8245cb4
Paul <paul.magalong0@gmail.com>
2026-06-07 01:26:01 +0800

    Final edits for the modal and tried to fixed the font issues.

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
-// Get the modal
 var modal = document.getElementById("Modal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("Btn");
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+function openModal() {
+  if (modal) {
+    modal.style.display = "flex";
+    document.body.style.overflow = "hidden";
   }
 }
+
+function closeModal() {
+  if (modal) {
+    modal.style.display = "none";
+    document.body.style.overflow = "";
+  }
+}
+
+if (span) {
+  span.onclick = closeModal;
+}
+
+window.onclick = function(event) {
+  if (event.target === modal) {
+    closeModal();
+  }
+};
+
+document.addEventListener("keydown", function(e) {
+  if (e.key === "Escape" && modal && modal.style.display === "flex") {
+    closeModal();
+  }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+  document.querySelectorAll('a[href="#"]').forEach(function(link) {
+    link.addEventListener("click", function(e) {
+      e.preventDefault();
+      openModal();
+    });
+  });
+
+  openModal();
+});
 
 (function () {
   'use strict';
